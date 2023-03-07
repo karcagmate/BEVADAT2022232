35f7ffa76235d3de2a379f6447ad7c24af67c710
Máté <karcagmate@stud.uni-obuda.hu>
2023-03-07 21:55:42 +0100

    [INIT]

--- GYAK/GYAK02/GYAK02.py
+++ GYAK/GYAK02/GYAK02.py
@@ -1,3 +1,4 @@
+# %%
 import numpy as np
 #FONTOS!!!
 #  CSAK OTT LEHET HASZNÁLNI FOR LOOP-OT AHOL A FELADAT KÜLÖN KÉRI!
@@ -16,6 +17,11 @@ def create_array(input: tuple)->np.array:
 #input=(2,2)
 #create_array(input)
 
+
+
+
+
+# %%
 #Készíts egy függvényt ami a paraméterként kapott array-t főátlót feltölti egyesekkel
 #Be: [[1,2],[3,4]]
 #Ki: [[1,2],[3,1]]
@@ -27,6 +33,7 @@ def set_one(input:np.array)->np.array:
 #proba=np.array([[1,5],[3,5]])
 #print(set_one(proba))
 
+# %%
 # Transzponáld a paraméterül kapott mártix-ot:
 # Be: [[1, 2], [3, 4]]
 # Ki: [[1, 2], [3, 4]]
@@ -38,6 +45,7 @@ def do_transponse(input:np.array)->np.array:
 #proba=np.array([[1, 2], [5, 6]])
 #do_transponse(proba)
 
+# %%
 # Készíts egy olyan függvényt ami az array-ben lévő értékeket N tizenedjegyik kerekíti, alapértelmezetten 
 # Be: [0.1223, 0.1675], n = 2
 # Ki: [0.12, 0.17]
@@ -49,6 +57,8 @@ def round_array(input:np.array,num:int)->np.array:
 #proba=np.array([0.1223, 0.1675])
 #round_array(proba,2)
 
+
+# %%
 # Készíts egy olyan függvényt, ami a bementként  0 és 1 ből álló tömben a 0 - False-ra az 1 True-ra cserélni
 # Be: [[1, 0, 0], [1, 1, 1],[0, 0, 0]]
 # Ki: [[ True False False], [ True  True  True], [False False False]]
@@ -59,19 +69,23 @@ def bool_array(inarray:np.array)->np.array:
     return newarr
 #proba=np.array([[1, 0, 0], [1, 1, 1],[0, 0, 0]])
 #bool_array(proba)
+    
 
+
+# %%
 # Készíts egy olyan függvényt, ami a bementként  0 és 1 ből álló tömben a 1 - False-ra az 0 True-ra cserélni
 # Be: [[1, 0, 0], [1, 1, 1],[0, 0, 0]]
 # Ki: [[ True False False], [ True  True  True], [False False False]]
 # invert_bool_array()
 def invert_bool_array(array:np.array)->np.array:
-    newarr =np.invert(array.astype(bool))
+    newarr = array.astype(bool)
     
     return newarr
 
 #proba=np.array([[1, 0, 0], [1, 1, 1],[0, 0, 0]])
-#print(invert_bool_array(proba))
+#invert_bool_array(proba)
 
+# %%
 
 # Készíts egy olyan függvényt ami a paraméterként kapott array-t kilapítja
 # Be: [[1,2], [3,4]]
@@ -83,3 +97,7 @@ def flatten(input:np.array)->np.array:
 
 #proba=np.array([[1,2], [3,4]])
 #flatten(proba)
+    
+
+
+
---
+++ HAZI/HAZI02/HAZI02.py
@@ -0,0 +1,182 @@
+# %%
+import numpy as np
+
+# %%
+#Készíts egy olyan függvényt ami összehasonlít két array-t és adjon vissza egy array-ben, hogy hol egyenlőek 
+# Pl Be: [7,8,9], [9,8,7] 
+# Ki: [1]
+# compare_two_array()
+# egyenlő elemszámúakra kell csak hogy működjön
+def compare_two_array(input1:np.array,input2:np.array)->np.array:
+    return np.array_equal()    
+pl1=np.array([7,8,9])
+pl2=np.array([9,8,7] )
+compare_two_array(pl1,pl2)
+
+
+# %%
+#Készíts egy olyan függvényt, ami vissza adja a megadott array dimenzióit:
+# Be: [[1,2,3], [4,5,6]]
+# Ki: "sor: 2, oszlop: 3, melyseg: 1"
+# get_array_shape()
+# 3D-vel még műküdnie kell!
+def get_array_shape(input:np.array):
+    shape=input.shape
+    if len(shape)==1:
+        return f"sor: {shape[0]}, oszlop: 1, melyseg: 1"
+    elif len(shape)==2:
+        return f"sor: {shape[0]}, oszlop: {shape[1]}, melyseg: 1"
+    elif len(shape)==3:
+        return f"sor: {shape[0]}, oszlop: {shape[1]}, melyseg: {shape[2]}"
+    
+
+pl=np.array([[1,2,3],[1,2,3],[1,2,3]])
+get_array_shape(pl)
+
+# %%
+# Készíts egy olyan függvényt, aminek segítségével elő tudod állítani egy neurális hálózat tanításához szükséges Y-okat egy numpy array-ből. 
+#Bementként add meg az array-t, illetve hogy mennyi class-od van. Kimenetként pedig adjon vissza egy 2d array-t, ahol a sorok az egyes elemek. Minden nullákkal teli legyen és csak ott álljon egyes, ahol a bementi tömb megjelöli
+# Be: [1, 2, 0, 3], 4
+# Ki: [[0,1,0,0], [0, 0, 1, 0], [1, 0, 0, 0], [0, 0, 0, 1]]
+# encode_Y()
+def encode_Y(input:np.array,class_num:int)->np.array:
+    num_sample=input.shape[0]
+    helper=np.zeros((num_sample,class_num))
+    for i in range(num_sample):
+        helper[i,int(input[i])]=1
+    return helper
+
+pl=np.array([1, 2, 0, 3])
+print(encode_Y(pl,4))
+
+# %%
+# A fenti feladatnak valósítsd meg a kiértékelését. Adj meg a 2d array-t és adj vissza a decodolt változatát
+# Be:  [[0,1,0,0], [0, 0, 1, 0], [1, 0, 0, 0], [0, 0, 0, 1]]
+# Ki:  [1, 2, 0, 3]
+# decode_Y()
+
+# %%
+# Készíts egy olyan függvényt, ami képes kiértékelni egy neurális háló eredményét! Bemenetként egy listát és egy array-t és adja vissza a legvalószínübb element a listából.
+# Be: ['alma', 'körte', 'szilva'], [0.2, 0.2, 0.6]. 
+# Ki: 'szilva'
+# eval_classification()
+def eval_classification(input1:list, input2:np.array)->str:
+    index=np.argmax(input2)
+    return input1[index]
+
+pl1=['alma', 'körte', 'szilva']
+pl2=np.array([0.2, 0.2, 0.6])
+eval_classification(pl1,pl2)
+
+
+
+# %%
+# Készíts egy olyan függvényt, ahol az 1D array-ben a páratlan számokat -1-re cseréli
+# Be: [1,2,3,4,5,6]
+# Ki: [-1,2,-1,4,-1,6]
+# repalce_odd_numbers()
+def replace_odd_numbers(input:np.array)->np.array:
+    return np.where(input%2==1,-1,input)
+
+pl=np.array([1,2,3,4,5,6])
+replace_odd_numbers(pl)
+
+# %%
+# Készíts egy olyan függvényt, ami egy array értékeit -1 és 1-re változtatja, attól függően, hogy az adott elem nagyobb vagy kisebb a paraméterként megadott számnál.
+# Ha a szám kisebb mint a megadott érték, akkor -1, ha nagyobb vagy egyenlő, akkor pedig 1.
+# Be: [1, 2, 5, 0], 2
+# Ki: [-1, 1, 1, -1]
+# replace_by_value()
+def replace_by_value(input:np.array,treshold:int)->np.array:
+    input[input<treshold]=-1
+    input[input>=treshold]=1
+    return input
+
+pl=np.array([1, 2, 5, 0])
+replace_by_value(pl,2)
+
+
+# %%
+# Készítsd egy olyan függvényt, ami az array értékeit összeszorozza és az eredmény visszaadja
+# Be: [1,2,3,4]
+# Ki: 24
+# array_multi()
+# Ha több dimenziós a tömb, akkor az egész tömb elemeinek szorzatával térjen vissza
+def array_multi(input:np.array):
+    return np.prod(input)
+
+pl=np.array([1,2,3,4])
+array_multi(pl)
+
+# %%
+# Készítsd egy olyan függvényt, ami a 2D array értékeit összeszorozza és egy olyan array-el tér vissza, aminek az elemei a soroknak a szorzata
+# Be: [[1, 2], [3, 4]]
+# Ki: [2, 12]
+# array_multi_2d()
+def array_multi_2d(input:np.array)->np.array:
+    rows=np.prod(input,axis=1)
+    return rows
+
+pl=np.array([[1, 2], [3, 4]])
+array_multi_2d(pl)
+
+# %%
+# Készíts egy olyan függvényt, amit egy meglévő numpy array-hez készít egy bordert nullásokkal. Bementként egy array-t várjon és kimenetként egy array jelenjen meg aminek van border-je
+# Be: [[1,2],[3,4]]
+# Ki: [[0,0,0,0],[0,1,2,0],[0,3,4,0],[0,0,0,0]]
+# add_border()
+def add_border(input:np.array)->np.array:
+    ori=input.shape
+    border=(ori[0]+2,ori[1]+2)
+    out=np.zeros(border)
+    out[1:-1,1:-1]=input
+    return out
+
+pl=np.array([[1,2],[3,4]])
+add_border(pl)
+
+# %%
+# Készíts egy olyan függvényt ami két dátum között felsorolja az összes napot.
+# Be: '2023-03', '2023-04'
+# Ki: ['2023-03-01', '2023-03-02', .. , '2023-03-31',]
+# list_days()
+from datetime import timedelta,datetime
+def list_days(start,finish):
+    starter = datetime.strptime(start, '%Y-%m')
+    end = datetime.strptime(finish, '%Y-%m')
+    delta=timedelta(days=1)
+    out=[]
+    while starter<=end:
+        out.append(starter.strftime('%Y-%m-%d'))
+        starter+=delta
+    return out
+
+starti='2023-03'
+fin='2023-04'
+list_days(starti,fin)
+
+
+
+
+# %%
+ #Írj egy fügvényt ami vissza adja az aktuális dátumot az alábbi formában: YYYY-MM-DD
+# Be:
+# Ki: 2017-03-24 
+from datetime import datetime
+def datetime_now():
+    return datetime.now().strftime('%Y-%m-%d')
+
+datetime_now()
+
+# %%
+# Írj egy olyan függvényt ami visszadja, hogy mennyi másodperc telt el 1970 január 01. 00:00:00 óta.
+# Be: 
+# Ki: másodpercben az idó, int-é kasztolva
+# sec_from_1970()
+import time
+def sec_from_1970()->int:
+    return int(time.time())
+
+sec_from_1970()
+
+
