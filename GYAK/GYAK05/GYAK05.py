039a33dcfca2525971be21c4534cf823efad7372
Máté <karcagmate@stud.uni-obuda.hu>
2023-03-30 13:02:27 +0200

    update

--- GYAK/GYAK05/GYAK05.py
+++ GYAK/GYAK05/GYAK05.py
@@ -2,7 +2,7 @@ import numpy as np
 
 from typing import Tuple
 from scipy.stats import mode
-from sklearn.metrics import confusion_matrix, euclidean_distances
+from sklearn.metrics import confusion_matrix
 import seaborn as sns
 #path="iris.csv"
 class KNNClassifier:
@@ -11,14 +11,20 @@ class KNNClassifier:
     def __init__(self,k:int,test_split_ratio :float) -> None:
         self.k=k
         self.test_split_ratio=test_split_ratio
+        self.x_train=None
+        self.y_train=None
+        self.x_test=None
+        self.y_test=None
+        self.y_preds=None
     @staticmethod
-    def loadcsv(path)->Tuple[np.ndarray,np.ndarray]:
+    def loadcsv(path:str)->Tuple[np.ndarray,np.ndarray]:
         np.random.seed(42)
         dataset=np.genfromtxt(path,delimiter=',')
         np.random.shuffle(dataset)
         x,y=dataset[:,:-1],dataset[:,-1]
+
         return x,y
-    @classmethod
+    
     def train_test_split(featrues:np.ndarray,labels:np.ndarray,self)->Tuple[np.ndarray,np.ndarray,np.ndarray]:
         test_size=int(len(featrues)*self.test_split_ratio)
         train_size=len(featrues)-test_size
@@ -31,23 +37,24 @@ class KNNClassifier:
     def euclidan(element_of_x:np.ndarray,self)->np.ndarray:
       return np.sqrt(np.sum((self.x_train-element_of_x)**2,axis=1))
     
-    @classmethod
+    
     def perdict(x_test:np.ndarray,self)->np.ndarray:
         labels_pred=[]
         for x_test_element in x_test:
-            distances= euclidean_distances(self.x_train,x_test_element)
+            distances= self.euclidean(x_test_element)
             distances=np.array(sorted(zip(distances,self.y_train)))
             label_pred=mode(distances[:self.k,1],keepdims=False).mode
             self.y_pred.append(label_pred)
         return  np.ndarray(labels_pred,dtype=np.int64)
-    @classmethod
+    
     def accuracy(self) -> float:
         true_positive = (self.y_test == self.y_preds).sum()
         return true_positive / len(self.y_test) * 100
-    @classmethod
-    def plot_confusion_matrix(self):
-        conf_matrix = confusion_matrix(self.y_test,self.y_preds)
-        sns.heatmap(conf_matrix,annot=True)
+    
+    def confusion_matrix(self):
+        return confusion_matrix(self.y_test,self.y_preds)
+    
+    @property
     def k_neighbors(self):
         return self.k
 
