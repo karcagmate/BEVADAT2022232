172ca8478239368414ecb9e7d8bc168b0d697d76
Máté <karcagmate@stud.uni-obuda.hu>
2023-04-02 21:37:47 +0200

    update

--- GYAK/GYAK05/GYAK05.py
+++ GYAK/GYAK05/GYAK05.py
@@ -11,11 +11,11 @@ class KNNClassifier:
     def __init__(self,k:int,test_split_ratio :float) -> None:
         self.k=k
         self.test_split_ratio=test_split_ratio
-        self.x_train=None
-        self.y_train=None
-        self.x_test=None
-        self.y_test=None
-        self.y_preds=None
+        #self.x_train=None
+        #self.y_train=None
+        #self.x_test=None
+        #self.y_test=None
+        #self.y_preds=None
     @property
     def k_neighbors(self):
         return self.k
@@ -24,39 +24,40 @@ class KNNClassifier:
     def loadcsv(path:str)->Tuple[np.ndarray,np.ndarray]:
         np.random.seed(42)
         dataset=np.genfromtxt(path,delimiter=',')
-        np.random.shuffle(dataset)
-        x,y=dataset[:,:-1],dataset[:,-1]
+        np.random.shuffle(dataset,)
+        x,y=dataset[:,:4],dataset[:,-1]
 
         return x,y
     
-    def train_test_split(self,featrues:np.ndarray,labels:np.ndarray)->None:
+    def train_test_split(self,featrues:np.ndarray,labels:np.ndarray):
         test_size=int(len(featrues)*self.test_split_ratio)
         train_size=len(featrues)-test_size
         assert len(featrues)==test_size+train_size,"Size mismatch!"
         x_train,y_train=featrues[:train_size,:],labels[:train_size]
-        x_test,y_test=featrues[train_size:,:],labels[train_size:]
-        self.x_train,self.y_train=x_train,y_train
+        x_test,y_test=featrues[train_size:train_size+test_size,:],labels[train_size:train_size+test_size]
 
+        self.x_train,self.y_train=x_train,y_train
         self.x_test,self.y_test=x_test,y_test
     
-    def euclidan(self,element_of_x:np.ndarray)->np.ndarray:
+    def euclidean(self,element_of_x:np.ndarray)->np.ndarray:
       return np.sqrt(np.sum((self.x_train-element_of_x)**2,axis=1))
     
     
-    def perdict(self,x_test:np.ndarray)->np.ndarray:
+    def perdict(self,x_test:np.ndarray):
         labels_pred=[]
         for x_test_element in x_test:
             distances= self.euclidean(x_test_element)
             distances=np.array(sorted(zip(distances,self.y_train)))
             label_pred=mode(distances[:self.k,1],keepdims=False).mode
-            self.y_pred.append(label_pred)
-        return  np.ndarray(labels_pred,dtype=np.int64)
+            labels_pred.append(label_pred)
+            self.y_preds=np.array(labels_pred,dtype=np.int32)
+        return  self.y_preds
     
     def accuracy(self) -> float:
         true_positive = (self.y_test == self.y_preds).sum()
         return true_positive / len(self.y_test) * 100
     
-    def confusion_matrix(self):
+    def confusion_matrix(self)->np.ndarray:
         return confusion_matrix(self.y_test,self.y_preds)
     
    
