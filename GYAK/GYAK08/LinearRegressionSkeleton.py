3f15e2f09993a7815f3e49a7d75b62523fe63fc9
Máté <karcagmate@stud.uni-obuda.hu>
2023-04-24 20:06:09 +0200

    update

--- GYAK/GYAK08/LinearRegressionSkeleton.py
+++ GYAK/GYAK08/LinearRegressionSkeleton.py
@@ -1,27 +1,57 @@
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression as lr
 
 
 class LinearRegression:
-    def __init__(self, epochs: int = 1000, lr: float = 1e-3):
+    def __init__(self, dataset : pd.DataFrame, epochs: int = 1000, lr: float = 1e-3):
         self.epochs = epochs
         self.lr = lr
-        
-    
+        self.dataset = dataset
+        self.X_train=None
+        self.y_train=None
+        self.X_test=None
+        self.y_test=None
 
+    def split(self,X,y):
+        X_train, X_test, y_train, y_test= train_test_split(X, y, test_size=0.2, random_state=42)
+        self.X_train=X_train
+        self.X_test=X_test
+        self.y_train=y_train
+        self.y_test=y_test
+        return self.X_train,self.X_test,self.y_train,self.y_test
 
-    def fit(self,X:np.array,y:np.array):
-    
-        model=lr.fit(X,y)
-        return model
-        
+    def fit(self, X: np.array, y: np.array):
+         
 
-        
+        m = 0
+        c = 0
 
-        
+        self.L = 0.2  # The learning Rate
+        self.epochs = 1000  # The number of iterations to perform gradient descent
 
-    def predict(self, X,model):
-       y_pred=model.predict(X)
-       return y_pred
+        n = float(len(self.X_train)) # Number of elements in X
+
+        # Performing Gradient Descent 
+        losses = []
+        for i in range(self.epochs): 
+            y_pred = m*self.X_train + c  # The current predicted value of Y
+
+            residuals = y_pred - self.y_train
+            loss = np.sum(residuals ** 2)
+            losses.append(loss)
+            D_m = (-2/n) * sum(self.X_train * residuals)  # Derivative wrt m
+            D_c = (-2/n) * sum(residuals)  # Derivative wrt c
+            self.m = m + self.L * D_m  # Update m
+            self.c = c + self.L * D_c  # Update c
+
+       
+
+    def predict(self, X):
+        pred = []
+        for self.X in self.X_test:
+            self.y_pred = self.m*X + self.c
+            pred.append(self.y_pred)
+
+        self.y_pred = self.m*self.X_test + self.c
+        return self.y_pred
--- GYAK/GYAK08/Test.py
+++ GYAK/GYAK08/Test.py
@@ -8,11 +8,11 @@ iris=load_iris()
 data=pd.DataFrame(iris.data,columns=iris.feature_names)
 X=data['petal width (cm)'].values
 y=data['sepal length (cm)'].values
-Lr=LinearRegression()
+Lr=LinearRegression(data)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-model=Lr.fit(X_test,y_test)
-pred=Lr.predict(X_test,model)
+X_train, X_test, y_train, y_test = Lr.split(X,y)
+Lr.fit(X_test,y_test)
+pred=Lr.predict(X)
 print(pred)
 plt.scatter(X_test,y_test)
 plt.plot([min(X_test), max(X_test)], [min(pred), max(pred)], color='red')
